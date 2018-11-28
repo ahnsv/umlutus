c35df5e0be43f221e83725a7efc55a60c15843cc
SeungJun Lee <17-70011816@af.mil>
2018-11-28 11:32:19 +0900

    malicious dependency removed, ran plantuml for the first time

--- dist/umlBuilder.js
+++ dist/umlBuilder.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var node_plantuml_1 = __importDefault(require("node-plantuml"));
+var fs_1 = __importDefault(require("fs"));
 function buildUml() { }
 exports.buildUml = buildUml;
 function buildGeneralization() { }
@@ -16,3 +21,5 @@ function buildAggregation() { }
 exports.buildAggregation = buildAggregation;
 function buildComposition() { }
 exports.buildComposition = buildComposition;
+var gen = node_plantuml_1.default.generate("./example/input-file.puml");
+gen.out.pipe(fs_1.default.createWriteStream("output-file.png"));
--- src/umlBuilder.ts
+++ src/umlBuilder.ts
@@ -9,3 +9,6 @@ export function buildAssociation() { }
 export function buildDirectAssociation() { }
 export function buildAggregation() { }
 export function buildComposition() { }
+
+const gen = plantuml.generate("./example/input-file.puml")
+gen.out.pipe(fs.createWriteStream("output-file.png"))
